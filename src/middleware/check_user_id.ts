61fafeac2542c467d7e0d057f5ee4a9aeddac445
alvindimas05 <alvin.dimas.praditya@gmail.com>
2023-05-31 14:20:23 +0700

    Removing paths and adding manual to routes instead

--- src/middleware/check_user_id.ts
+++ src/middleware/check_user_id.ts
@@ -1,12 +1,9 @@
 import { User } from "@mongoose";
 import express from "express";
 
-const paths = /(follow|profil)/g;
 export default async function (req: express.Request, res: express.Response, next: express.NextFunction){
-    if(paths.test(req.url) && req.method != "GET"){
-        if(!("user_id" in req.body)) return res.err("Missing user_id!")
-        let user = await User.countDocuments({ _id: req.body.user_id });
-        if(user < 1) return res.err("User not found!");
-    }
+    if(!("user_id" in req.body)) return res.err("Missing user_id!")
+    let user = await User.countDocuments({ _id: req.body.user_id });
+    if(user < 1) return res.err("User not found!");
     next();
 }
