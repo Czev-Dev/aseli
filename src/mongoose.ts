c581793f414fae6506b5ad40a07ae0ffd7e4acb9
alvindimas05 <alvin.dimas.praditya@gmail.com>
2023-05-31 14:19:06 +0700

    Adding post model

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -4,9 +4,17 @@ let env = process.env;
 connect(`mongodb://${env.DB_HOST}`, { dbName: env.DB_NAME })
 .then(() => console.log("Connected to database!"));
 
-export const User = model("User", new Schema({
+export const User = model("user", new Schema({
     username: String,
     password: String,
+    postCooldown: {
+        type: Boolean,
+        default: false 
+    },
+    commentCooldown: {
+        type: Boolean,
+        default: false 
+    },
     description: {
         type: String,
         default: null
@@ -22,5 +30,33 @@ export const User = model("User", new Schema({
     profil: {
         type: String,
         default: null
+    }
+}));
+export const Post = model("post", new Schema({
+    userId: Schema.Types.ObjectId,
+    description: String,
+    imageName: String,
+    time: {
+        type: Date,
+        default: Date.now
+    },
+    ril: {
+        type: Array,
+        default: []
+    },
+    fek: {
+        type: Array,
+        default: []
+    },
+    comments: {
+        type: [{
+            username: String,
+            comment: String,
+            time: {
+                type: Date,
+                default: Date.now
+            }
+        }],
+        default: []
     }
 }));
