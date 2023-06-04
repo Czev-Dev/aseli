95413e020c65d064a6c18540d37fbedf81f0d674
alvindimas05 <alvin.dimas.praditya@gmail.com>
2023-06-04 19:04:30 +0700

    Changing import and add routes for ril fek

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
-import "./mongoose";
-import check_body from "./middleware/check_body";
-import check_user_id from "./middleware/check_user_id";
-import upload from "./middleware/multer";
-import user from "./routes/user";
-import post from "./routes/post"
+import "@mongoose";
+import check_body from "@middleware/check_body";
+import check_user_id from "@middleware/check_user_id";
+import upload from "@middleware/multer";
+import user from "@routes/user";
+import post from "@routes/post"
+import check_post_id from "@middleware/check_post_id";
 
 const app = express();
 app.use("/uploads", express.static("./uploads"));
@@ -16,6 +17,8 @@ app.use(check_body);
 // Routes
 app.get("/post/:page?", post.get);
 app.post("/post", [upload.single("image"), check_user_id, check_body], post.post);
+app.post("/post/ril", [check_user_id, check_post_id], post.ril);
+app.post("/post/fek", [check_user_id, check_post_id], post.fek);
 
 app.post("/user/login", user.login);
 app.post("/user/register", user.register);
