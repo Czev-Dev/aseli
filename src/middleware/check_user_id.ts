a6971a900fd8018c10eb5ae6f01608c1070321da
alvindimas05 <alvin.dimas.praditya@gmail.com>
2023-06-04 19:05:06 +0700

    Check if user id is a valid id

--- src/middleware/check_user_id.ts
+++ src/middleware/check_user_id.ts
@@ -2,7 +2,7 @@ import { User } from "@mongoose";
 import express from "express";
 
 export default async function (req: express.Request, res: express.Response, next: express.NextFunction){
-    if(!("user_id" in req.body)) return res.err("Missing user_id!")
+    if(!("user_id" in req.body) || req.body.user_id.length < 24) return res.err("Missing user_id!")
     let user = await User.countDocuments({ _id: req.body.user_id });
     if(user < 1) return res.err("User not found!");
     next();
